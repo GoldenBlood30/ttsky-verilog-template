b6ab5e4f72c375ac5b1794b336588ca20dee8c54
GoldenBlood30 <dixitawtansh@gmail.com>
2026-09-23 01:37:09 +0530

    Update Forwarding_unit.v

--- src/Forwarding_unit.v
+++ src/Forwarding_unit.v
@@ -13,7 +13,7 @@ always @ (*)
 begin
 forward_rs=2'b00;
 forward_rt=2'b00;
-if(EX_MEM_Reg_write)
+if(EX_MEM_Reg_write && (EX_MEM_rd != 5'd0))   // r0 is hard-wired to 0: never forward a write to it
 begin
 if(EX_MEM_rd==ID_EX_rs)
 begin
@@ -24,7 +24,7 @@ begin
 forward_rt=2'b10;
 end
 end
-if (MEM_WB_Reg_write)
+if (MEM_WB_Reg_write && (MEM_WB_rd != 5'd0))   // r0 is hard-wired to 0: never forward a write to it
 begin
 if((MEM_WB_rd==ID_EX_rs)&&(~(EX_MEM_rd==ID_EX_rs)||~EX_MEM_Reg_write))
 begin
